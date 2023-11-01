28904673d1bb8d0d56a99ff02d02a3062b7fca99
LgnAA <lgnzxcaa@gmail.com>
2023-11-01 08:33:14 +0300

    1st commit

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+
+let money = 300;
+let income = 10;
+let addExpenses = 30;
+let deposit = 100;
+let mission = 1000;
+let period = 50;
+const pi = 3.14;
+alert('hello world');
+console.log('money: ', money);
